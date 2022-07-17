eaf07af3f92b08943cd951c906d1fc792e5a5e05
BOOK-153M5AROP9\kimra <kimrawun@naver.com>
2022-07-17 20:35:06 +0900

    '.'

--- app-prac/app/src/main/java/com/bitcamp/board/App.java
+++ app-prac/app/src/main/java/com/bitcamp/board/App.java
@@ -3,9 +3,105 @@
  */
 package com.bitcamp.board;
 
+import java.util.Date;
+import java.util.Scanner;
+
 public class App {
   public static void main(String[] args) {
+    System.out.println();
     System.out.println("[게시판 애플리케이션]");
+    System.out.println();
     System.out.println("환영합니다!");
+    System.out.println();
+
+    Scanner keyboardInput = new Scanner(System.in);
+
+    int no = 0;
+    String title = "";
+    String content = "";
+    String writer = "";
+    String password = "";
+    int viewCount = 0;
+    long createdDate = 0;
+
+    while (true) {
+      System.out.println("----------------------------------------------------");
+      System.out.println();
+      System.out.println("메뉴:");
+      System.out.println("   1 - 게시글 목록");
+      System.out.println("   2 - 게시글 상세보기");
+      System.out.println("   3 - 게시글 등록");
+      System.out.println();
+      System.out.print("메뉴를 선택하세요! [1..3] (0: EXIT)");
+
+      int menuNo = keyboardInput.nextInt();
+      keyboardInput.nextLine();
+
+      if (menuNo == 0) {
+        break;
+      } else if (menuNo == 1) {
+        System.out.println("---------------------------------------------------");
+        System.out.println("[게시글 목록]");
+        System.out.println();
+        System.out.println("번호       제목       조회수    작성자    등록일");
+        System.out.println();
+
+        System.out.print(1);
+        System.out.print("\t");
+        System.out.print("제목입니다1");
+        System.out.print("\t");
+        System.out.print(20 + "\t");
+        System.out.print("홍길동\t");
+        System.out.print("2022-07-17\r\n");
+
+        System.out.print(2 + "\t" + "제목입니다2\t" +
+            11 + "\t" + "홍길동\t" + "2022-07-17\n");
+
+        System.out.println(3 + "\t제목입니다3\t" +
+            31 + "\t" + "임꺽정\t2022-07-17");
+
+        System.out.printf("%d\t%s\t%d\t%s\t%s\n",
+            4, "제목입니다4", 45, "유관순", "2022-07-17");
+
+      } else if (menuNo == 2) {
+        System.out.println("---------------------------------------------------");
+        System.out.println("[게시판 상세보기]");
+
+        System.out.printf("번호: %d\n", no);
+        System.out.printf("제목: %s\n", title);
+        System.out.printf("내용: %s\n", content);
+        System.out.printf("조회수: %d\n", viewCount);
+        System.out.printf("작성자: %s\n", writer);
+
+        Date date = new Date(createdDate);
+        System.out.printf("등록일: %tY-%1$tm-%1$tm-%1$td %1$tH:%1$tM\n", date);
+
+      } else if (menuNo == 3) {
+        System.out.println("---------------------------------------------------");
+        System.out.println("[게시글 등록]");
+
+        System.out.print("제목 => ");
+        title = keyboardInput.nextLine();
+
+        System.out.print("내용 => ");
+        content = keyboardInput.nextLine();
+
+        System.out.print("작성자 => ");
+        writer = keyboardInput.nextLine();
+
+        System.out.print("암호 => ");
+        password = keyboardInput.nextLine();
+
+        no = 1;
+        viewCount = 0;
+        createdDate = System.currentTimeMillis();
+
+      } else {
+        System.out.println("메뉴 번호가 옳지 않습니다!");
+      }
+    } // while end
+    System.out.println("---------------------------------------------------");
+    System.out.println("안녕히 가세요!");
+    keyboardInput.close();
   }
-}
+} 
